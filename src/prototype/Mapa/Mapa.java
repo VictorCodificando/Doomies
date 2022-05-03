603d04d63992960c1f480a50e7e81adddea84109
Victor <victor.gonzalez.cobos@iesjulianmarias.es>
2022-05-03 12:59:26 +0200

    Clases creadas

--- src/prototype/Mapa/Mapa.java
+++ src/prototype/Mapa/Mapa.java
@@ -1,5 +1,9 @@
 package prototype.Mapa;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import prototype.Entes.Seres.SerVivo;
+
 /**
  *
  * @author Víctor Zero
@@ -7,9 +11,22 @@ package prototype.Mapa;
 public class Mapa {
 
     private final Tile[][] tiles;
+    private SerVivo[] seres;
+    private BufferedImage background;
+    private int x;
+    private int y;
 
-    public Mapa(Tile[][] tiles) {
+    public Mapa(Tile[][] tiles, SerVivo[] seres) {
         this.tiles = tiles;
+        this.seres = seres;
+    }
+
+    public void dibujar(Graphics g) {
+
+    }
+
+    public void actualizar() {
+
     }
 
 }
